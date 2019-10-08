38a4f10258fd0a09c409d4c7735660cb3cb969c3
Simon Maher <simonmaher8307@hotmail.com>
2019-10-08 10:14:32 +0300

    created event listener for navbar btn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const navbarBtn = document.querySelector('.navbar__btn')
 const navbarLinks = document.querySelector('.navbar__links')
 
+navbarBtn.addEventListener('click', function () {
+    let value = navbarLinks.classList.contains('navbar__collapse')
+
+    
+})
